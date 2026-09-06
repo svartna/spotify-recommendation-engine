6fa4f92217983e8962939415d06959aa750040c0
Christian de los Rios <cdelosriosm@hotmail.com>
2026-09-06 00:03:45 -0400

    feat: add curated genre data set

---
+++ curated_genres.py
@@ -0,0 +1,18 @@
+ARTIST_GENRES = {
+  "5INjqkS1o8h1imAzPqGZBb":["psychedelic rock", "neo-psychedelia", "psychedelic pop"], # tame impala
+  "5RTLRtXjbXI2lSXc6jxlAz":["contemporary r&b", "alternative r&b", "neo-soul"], # ravyn lenae
+  "4Gso3d4CscCijv0lmajZWs":["contemporary r&b", "hip-hop", "trap"], # don toliver
+  "2VIZDPesAdDk0zppWuy70U":["pop", "anime cover", "japanese rock"], # one project
+  "4dpARuHxo51G3z768sgnrY":["pop", "soul", "r&b"], # adele
+  "6eUKZXaKkcviH0Ku9w2n3V":["pop", "folk-pop", "soft rock"], # ed sheeran
+  "1iCnM8foFssWlPRLfAbIwo":["indie folk", "indie pop", "alternative"], # gigi perez
+  "00FQb4jTyendYWaN8pK0wa":["alternative pop", "baroque pop", "dream pop"], # lana del rey
+  "45dkTj5sMRSjrmBSBeiHym":["pop", "dance-pop", "contemporary r&b"], # tate mcrae
+  "3eVa5w3URK5duf6eyVDbu9":["pop", "alternative pop", "synth-pop"], # rosé 
+  "53XhwfbYqKCa1cC15pYq2q":["pop rock", "alternative rock", "indie"], # imagine dragons
+  "6qqNVTkY8uBg9cP3Jd7DAH":["alternative pop", "electropop", "dark pop"], # billie eilish
+  "0XHiH53dHrvbwfjYM7en7I":["grunge rock", "hard rock", "alternative metal"], # chris cornell
+  "0J7U24vlOOIeMpuaO6Q85A":["indie rock", "pop rock", "alternative pop"], # laura pergolizzi
+  "2wY79sveU1sp5g7SokKOiI":["pop", "soul", "contemporary r&b"], # sam smith 
+  "1snhtMLeb2DYoMOcVbb8iB":["japanese rock", "indie rock", "electronic music"] # kenshi yonezu
+  }
