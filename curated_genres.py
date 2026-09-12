dc2bf4de7e3e40f29af450cd63d773d625eb733a
Christian de los Rios <cdelosriosm@hotmail.com>
2026-09-12 17:53:28 -0400

    feat: trim amount of genres per artist

--- curated_genres.py
+++ curated_genres.py
@@ -1,21 +1,21 @@
 # NOTE: I originally planned on using /v1/artists to get genre data, but spotify removed this endpoint in 2026
-# so I am using a manually curated genre mapping instead. see readme for more 
+#       so I am using a manually curated genre mapping instead. see readme for more 
 
 ARTIST_GENRES = {
-  "5INjqkS1o8h1imAzPqGZBb":["psychedelic rock", "neo-psychedelia", "psychedelic pop"], # tame impala
-  "5RTLRtXjbXI2lSXc6jxlAz":["contemporary r&b", "alternative r&b", "neo-soul"], # ravyn lenae
-  "4Gso3d4CscCijv0lmajZWs":["contemporary r&b", "hip-hop", "trap"], # don toliver
-  "2VIZDPesAdDk0zppWuy70U":["pop", "anime cover", "japanese rock"], # one project
-  "4dpARuHxo51G3z768sgnrY":["pop", "soul", "r&b"], # adele
-  "6eUKZXaKkcviH0Ku9w2n3V":["pop", "folk-pop", "soft rock"], # ed sheeran
-  "1iCnM8foFssWlPRLfAbIwo":["indie folk", "indie pop", "alternative"], # gigi perez
-  "00FQb4jTyendYWaN8pK0wa":["alternative pop", "baroque pop", "dream pop"], # lana del rey
-  "45dkTj5sMRSjrmBSBeiHym":["pop", "dance-pop", "contemporary r&b"], # tate mcrae
-  "3eVa5w3URK5duf6eyVDbu9":["pop", "alternative pop", "synth-pop"], # rosé 
-  "53XhwfbYqKCa1cC15pYq2q":["pop rock", "alternative rock", "indie"], # imagine dragons
-  "6qqNVTkY8uBg9cP3Jd7DAH":["alternative pop", "electropop", "dark pop"], # billie eilish
-  "0XHiH53dHrvbwfjYM7en7I":["grunge rock", "hard rock", "alternative metal"], # chris cornell
-  "0J7U24vlOOIeMpuaO6Q85A":["indie rock", "pop rock", "alternative pop"], # laura pergolizzi
-  "2wY79sveU1sp5g7SokKOiI":["pop", "soul", "contemporary r&b"], # sam smith 
-  "1snhtMLeb2DYoMOcVbb8iB":["japanese rock", "indie rock", "electronic music"] # kenshi yonezu
+  "5INjqkS1o8h1imAzPqGZBb":["psychedelic rock"], # tame impala
+  "5RTLRtXjbXI2lSXc6jxlAz":["alternative r&b", "neo-soul"], # ravyn lenae
+  "4Gso3d4CscCijv0lmajZWs":["trap"], # don toliver
+  "2VIZDPesAdDk0zppWuy70U":["j-rock"], # one project
+  "4dpARuHxo51G3z768sgnrY":["pop", "soul"], # adele
+  "6eUKZXaKkcviH0Ku9w2n3V":["pop"], # ed sheeran
+  "1iCnM8foFssWlPRLfAbIwo":["indie folk"], # gigi perez
+  "00FQb4jTyendYWaN8pK0wa":["alternative pop"], # lana del rey
+  "45dkTj5sMRSjrmBSBeiHym":["pop"], # tate mcrae
+  "3eVa5w3URK5duf6eyVDbu9":["pop"], # rosé 
+  "53XhwfbYqKCa1cC15pYq2q":["pop rock"], # imagine dragons
+  "6qqNVTkY8uBg9cP3Jd7DAH":["alternative pop"], # billie eilish
+  "0XHiH53dHrvbwfjYM7en7I":["grunge rock"], # chris cornell
+  "0J7U24vlOOIeMpuaO6Q85A":["indie rock"], # laura pergolizzi
+  "2wY79sveU1sp5g7SokKOiI":["pop", "soul"], # sam smith 
+  "1snhtMLeb2DYoMOcVbb8iB":["j-rock"] # kenshi yonezu
   }
